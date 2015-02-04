67f6eec80a06f45314729ec2abdd15ed0bea5708
Szernex <szernex@gmail.com>
2015-02-04 16:19:18 +0100

    * changed: switched from AWT to Swing * changed: added check to see if a webcam is actually connected * added: show message boxes in case of errors * updated README.md

--- src/main/java/org/szernex/wcss/Main.java
+++ src/main/java/org/szernex/wcss/Main.java
@@ -3,65 +3,81 @@ package org.szernex.wcss;
 import com.github.sarxos.webcam.Webcam;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.*;
 import java.util.Properties;
 
-public class Main extends Frame implements KeyListener, WindowListener
+public class Main extends JFrame implements KeyListener
 {
 	public static final String CONFIG_FILE = "wcss.properties";
-
-	private static final String ACTION_SNAPSHOT = "snapshot";
-	private static Main Instance;
 
 	private Properties Config;
 
 	public static void main(String[] args) throws IOException, InterruptedException
 	{
-		Instance = new Main();
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				Main app = new Main();
+			}
+		});
 	}
 
-	public Main() throws IOException
+	public Main()
 	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Main");
+		setSize(250, 100);
+
 		loadProperties();
 
 		setLayout(new GridLayout(1, 1));
 		addKeyListener(this);
-		addWindowListener(this);
 
-		Label label = new Label("Press ENTER to take a snapshot");
+		JLabel label = new JLabel("Press ENTER to take a snapshot");
+		label.setHorizontalAlignment(JLabel.CENTER);
 		add(label);
 
-		setTitle("WebcamSnapshot");
-		setSize(200, 200);
 		setVisible(true);
 	}
 
-	private void loadProperties() throws IOException
+	private void loadProperties()
 	{
-		File file = new File(CONFIG_FILE);
-
-		if (!file.exists())
+		try
 		{
-			if (!file.createNewFile())
+			File file = new File(CONFIG_FILE);
+
+			if (!file.exists())
 			{
-				throw new IOException("could not create new properties file");
+				if (!file.createNewFile())
+				{
+					throw new IOException("could not create new properties file");
+				}
 			}
+
+			InputStream input = new FileInputStream(file);
+			Config = new Properties();
+
+			Config.load(input);
+			input.close();
+
+			OutputStream output = new FileOutputStream(file);
+
+			Config.setProperty("application", Config.getProperty("application", "mspaint %1"));
+			Config.store(output, "");
+			output.close();
 		}
-
-		InputStream input = new FileInputStream(file);
-		Config = new Properties();
-
-		Config.load(input);
-		input.close();
-
-		OutputStream output = new FileOutputStream(file);
-
-		Config.setProperty("application", Config.getProperty("application", "mspaint %1"));
-
-		Config.store(output, "");
-		output.close();
+		catch (IOException ex)
+		{
+			JOptionPane.showMessageDialog(null, "Something went wrong:\n" + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			System.err.println("Error: " + ex.getMessage());
+			ex.printStackTrace();
+		}
 	}
 
 	public void takeSnapshot()
@@ -72,19 +88,27 @@ public class Main extends Frame implements KeyListener, WindowListener
 			file.deleteOnExit();
 
 			Webcam webcam = Webcam.getDefault();
-			webcam.setViewSize(new Dimension(640, 480));
+
+			if (webcam == null)
+			{
+				JOptionPane.showMessageDialog(null, "No webcam found, aborting", "Error", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+
+			webcam.setViewSize(webcam.getViewSizes()[webcam.getViewSizes().length - 1]);
 			webcam.open();
 			ImageIO.write(webcam.getImage(), "PNG", file);
 			webcam.close();
 
 			String application = Config.getProperty("application").replace("%1", file.getAbsolutePath());
-			System.out.println("Executing " + application);
+			System.out.println("Executing: " + application);
 
 			Runtime runtime = Runtime.getRuntime();
 			runtime.exec(application);
 		}
 		catch (IOException ex)
 		{
+			JOptionPane.showMessageDialog(null, "Something went wrong:\n" + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
 			System.err.println("Error: " + ex.getMessage());
 			ex.printStackTrace();
 		}
@@ -99,8 +123,6 @@ public class Main extends Frame implements KeyListener, WindowListener
 	@Override
 	public void keyPressed(KeyEvent keyEvent)
 	{
-		System.out.println(keyEvent.getKeyCode());
-
 		if (keyEvent.getKeyCode() == KeyEvent.VK_ENTER)
 		{
 			takeSnapshot();
@@ -109,48 +131,6 @@ public class Main extends Frame implements KeyListener, WindowListener
 
 	@Override
 	public void keyReleased(KeyEvent keyEvent)
-	{
-
-	}
-
-	@Override
-	public void windowOpened(WindowEvent windowEvent)
-	{
-
-	}
-
-	@Override
-	public void windowClosing(WindowEvent windowEvent)
-	{
-		System.exit(0);
-	}
-
-	@Override
-	public void windowClosed(WindowEvent windowEvent)
-	{
-
-	}
-
-	@Override
-	public void windowIconified(WindowEvent windowEvent)
-	{
-
-	}
-
-	@Override
-	public void windowDeiconified(WindowEvent windowEvent)
-	{
-
-	}
-
-	@Override
-	public void windowActivated(WindowEvent windowEvent)
-	{
-
-	}
-
-	@Override
-	public void windowDeactivated(WindowEvent windowEvent)
 	{
 
 	}
